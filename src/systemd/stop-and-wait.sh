b93c4fd0a32a6c2f6397a854c1cc4219b1520ad2
Nepomuk Fraedrich <info@nepda.eu>
2018-01-03 18:00:16 +0100

    Added systemd service stopper-helper (ConsistsOf)

---
+++ src/systemd/stop-and-wait.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+serviceName=$1
+
+if [ "${serviceName}" = "" ] || [ "${serviceName}" = "--help" ]
+then
+    echo "Usage $0 [service-name.service]"
+    echo ""
+    echo "Stops the systemd user services and waits for real end of processes"
+    echo "Stops also all services which ConsistsOf this one (recursively)"
+    exit 1
+fi
+
+ExecMainPID=$(systemctl --user show ${serviceName} -p ExecMainPID | cut -d '=' -f 2)
+list=$(systemctl --user show ${serviceName} -p ConsistsOf | cut -d '=' -f 2)
+
+# recursive stopping ConsistsOf services
+if [ "$list" != "" ]
+then
+    for s in ${list}
+    do
+        bash $0 ${s}
+    done
+fi
+
+echo "PID ${serviceName}: ${ExecMainPID}"
+echo "Stop ${serviceName} service"
+systemctl --user stop ${serviceName}
+echo "Waiting for ${serviceName} to be stopped"
+if [ "${ExecMainPID}" -gt 0 ]
+then
+    while kill -0 ${ExecMainPID} 2> /dev/null;
+    do
+        echo -n "."
+        sleep 1
+    done
+fi
